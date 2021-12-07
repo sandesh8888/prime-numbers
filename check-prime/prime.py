3d998b21cf5d4fc77ef33d66e9fae63096513139
Sandesh Dhakal <48124330+sandesh8888@users.noreply.github.com>
2021-12-07 09:09:55 +0545

    add files

---
+++ check-prime/prime.py
@@ -0,0 +1,11 @@
+def check_prime(number):
+    if number>1:
+        for i in range(2,number):
+            if (number % i) == 0:
+                print(False)
+                break
+        else:
+            print(True)
+            
+    else:
+        print(False)
